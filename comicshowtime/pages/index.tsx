a3fd282f86a2b1df281ad45ddac310232e75a40c
Ramunmi <raruznh@gmail.com>
2021-01-27 15:34:38 -0300

    Inclusão de MetaTags para redes sociais

--- comicshowtime/pages/index.tsx
+++ comicshowtime/pages/index.tsx
@@ -6,11 +6,19 @@ import  QuizContainer  from "../src/components/QuizContainer";
 import QuizBackground from "../src/components/QuizBackground";
 import Footer from "../src/components/Footer";
 import GitHubCorner from "../src/components/GitHubCorner";
+import Head from "next/head"
 
 export default function Home() {
   // No arquivo index, deve existir uma funcao default para ser exportada, dessa forma o next saberá montar a tela
   return (
+
     <Global>
+      <Head>
+        <title>Comic show quiz!</title>
+        <meta property="og:image" content={db.bg}/>
+        <meta property="og:description" content="Teste seus conhecimentos sobre séries de comédia"/>
+        <meta property="og:title" content="Comic show Quiz!"/>
+      </Head>
       <QuizBackground backgroundImage={db.bg}>
         <QuizContainer>
           <QuizLogo />
